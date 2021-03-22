74d1f5b0ae8fa73b001b90d63e0578609062a791
unknown <zlinus.zachrison@gmail.com>
2021-03-22 16:48:21 +0100

    solving uppgift 1 and 3

---
+++ main.js
@@ -0,0 +1,26 @@
+//KOD 1
+
+let cars = ['volvo', 'saab', 'bmw', 'tesla']; 
+
+for(car in cars) {
+    console.log(cars[car]);
+}
+
+//KOD 2
+
+
+
+//let cars = ['volvo', 'saab', 'bmw', 'tesla'];  
+let fruits = ['apple', 'banana', 'orange'];
+
+//KOD 3
+
+let flowers = ['rose', 'tulip', 'lily', 'orchid'];
+
+function getFlowers(flower) {
+    for(flower in flowers) {
+        console.log(flowers[flower]);
+    }
+}
+
+getFlowers(flowers)

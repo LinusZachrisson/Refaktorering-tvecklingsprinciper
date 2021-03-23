2f0b95c1a3d0652daf1e4e6d59ea9c3f622d588a
unknown <zlinus.zachrison@gmail.com>
2021-03-23 20:40:50 +0100

    adding concat for cars and fruits array

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 //KOD 1
 
-let cars = ['volvo', 'saab', 'bmw', 'tesla']; 
+let cars = ["volvo", "saab", "bmw", "tesla"];
 
 // for(car in cars) {
 //     console.log(cars[car]);
 // }
 
-cars.forEach((car)=> {
-    console.log(car);
-})
+cars.forEach((car) => {
+  console.log(car);
+});
 
 //KOD 2
 
+//let cars = ['volvo', 'saab', 'bmw', 'tesla'];
+let fruits = ["apple", "banana", "orange"];
 
-
-//let cars = ['volvo', 'saab', 'bmw', 'tesla'];  
-let fruits = ['apple', 'banana', 'orange'];
+let carsAndFruits = cars.concat(fruits);
+console.log(carsAndFruits);
 
 //KOD 3
 
-let flowers = ['rose', 'tulip', 'lily', 'orchid'];
+let flowers = ["rose", "tulip", "lily", "orchid"];
 
 function getFlowers(flower) {
-    for(flower in flowers) {
-        console.log(flowers[flower]);
-    }
+  for (let i = 0; i < flower.length; i++) {
+    console.log(flower[i]);
+  }
 }
 
-getFlowers(flowers)
+getFlowers(flowers);
